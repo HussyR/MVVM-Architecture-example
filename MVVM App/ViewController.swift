248feac4c58dd9bce36ceacc949c84a59a5be8ae
Danila <danil.ryabikov@bk.ru>
2022-06-26 11:32:32 +0300

    Initial Commit

---
+++ MVVM App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM App
+//
+//  Created by Данил on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
